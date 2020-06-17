e8b3d7470a012631b4973306a65bba4a6c68ca4c
Pandacore <44470187+Pandacore@users.noreply.github.com>
2020-06-17 09:07:37 +0100

    Add RestController, axios and Allow Cross Origin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import FirstComponent from './components/learning-examples/FirstComponent';
-import SecondComponent from './components/learning-examples/SecondComponent';
-import ThirdComponent from './components/learning-examples/ThirdComponent';
-import FourthComponent from './components/learning-examples/FourthComponent';
-import Counter from "./components/counter/CounterButton";
+// import FirstComponent from './components/learning-examples/FirstComponent';
+// import SecondComponent from './components/learning-examples/SecondComponent';
+// import ThirdComponent from './components/learning-examples/ThirdComponent';
+// import FourthComponent from './components/learning-examples/FourthComponent';
+//import Counter from "./components/counter/CounterButton";
 import TodoApp from "./components/todo/TodoApp";
 
 import './App.css';
@@ -21,19 +21,19 @@ class App extends Component {
   }
 }
 
-class LearningComponents extends Component {
-    render() {
-        return (
-            //JSX always must have a parent component.
-            <div className="LearningComponents">
-                My Hello World
-                <FirstComponent></FirstComponent>
-                <SecondComponent></SecondComponent>
-                <ThirdComponent></ThirdComponent>
-                <FourthComponent></FourthComponent>
-            </div>
-        );
-    }
-}
+// class LearningComponents extends Component {
+//     render() {
+//         return (
+//             //JSX always must have a parent component.
+//             <div className="LearningComponents">
+//                 My Hello World
+//                 <FirstComponent></FirstComponent>
+//                 <SecondComponent></SecondComponent>
+//                 <ThirdComponent></ThirdComponent>
+//                 <FourthComponent></FourthComponent>
+//             </div>
+//         );
+//     }
+// }
 
 export default App;
--- frontend/src/components/todo/WelcomeComponent.jsx
+++ frontend/src/components/todo/WelcomeComponent.jsx
@@ -1,15 +1,42 @@
 import React, {Component} from "react";
 import {Link} from "react-router-dom";
+import HelloWorldService from "../../api/todo/HelloWorldService";
 
 export default class WelcomeComponent extends Component{
+    constructor() {
+        super();
+        this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this);
+        this.handleSuccessfulResponse = this.handleSuccessfulResponse.bind(this);
+        this.state = {
+            welcomeMessage : ''
+        }
+    }
     render() {
         return(
             <>
                 <h1>Welcome!</h1>
                 <div className="container">
-                    Welcome {this.props.match.params.name}. You can manage your todos <Link to="/todos">here</Link>
+                    Welcome {this.props.match.params.name}.
+                    You can manage your todos <Link to="/todos">here</Link>
+                </div>
+                <div className="container">
+                    Click here to get a customized welcome message.
+                    <button onClick={this.retrieveWelcomeMessage} className="btn btn-success">Get Welcome Message</button>
+                </div>
+                <div className="container">
+                    {this.state.welcomeMessage}
                 </div>
             </>
         )
+    }
+
+    retrieveWelcomeMessage(){
+        HelloWorldService.executeHelloWorldService().
+        then(response => this.handleSuccessfulResponse(response))
+        //catch()
+    }
+
+    handleSuccessfulResponse(response){
+        this.setState({welcomeMessage: response.data})
     }
 }
