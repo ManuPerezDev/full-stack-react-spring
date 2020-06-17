da97b5b86aabae23fab790f5a40b64c3fce793af
Pandacore <44470187+Pandacore@users.noreply.github.com>
2020-06-17 10:01:49 +0100

    Modify axios request to welcomePathVariable

--- frontend/src/components/todo/WelcomeComponent.jsx
+++ frontend/src/components/todo/WelcomeComponent.jsx
@@ -35,7 +35,10 @@ export default class WelcomeComponent extends Component{
         // HelloWorldService.executeHelloWorldService().
         // then(response => this.handleSuccessfulResponse(response))
         //catch()
-        HelloWorldService.executeHelloWorldBeanService().
+        // HelloWorldService.executeHelloWorldBeanService().
+        // then(response => this.handleSuccessfulResponse(response))
+
+        HelloWorldService.executeHelloWorldPathVariableService(this.props.match.params.name).
         then(response => this.handleSuccessfulResponse(response))
 
     }
