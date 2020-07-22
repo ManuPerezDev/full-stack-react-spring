0f7326e704f7b0e624a503b401c59cbbc06ed322
Pandacore <44470187+Pandacore@users.noreply.github.com>
2020-07-22 17:50:50 +0100

    Implemented JPA

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/Todo.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/Todo.java
@@ -1,10 +1,17 @@
 package com.in28minutes.rest.webservices.restfulwebservices.todo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Date;
 import java.util.Objects;
 
+@Entity
 public class Todo {
-    private long id;
+    @Id
+    @GeneratedValue
+    private Long id;
+
     private String username;
     private String description;
     private boolean isDone;
@@ -22,11 +29,11 @@ public class Todo {
         this.targetDate = targetDate;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoJpaResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoJpaResource.java
@@ -6,49 +6,49 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-
 import java.net.URI;
 import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
-public class TodoResource {
+public class TodoJpaResource {
     @Autowired
-    private TodoHardcodedService todoService;
+    private TodoJpaRepository todoJpaRepository;
 
-    @GetMapping("/users/{username}/todos")
+    @GetMapping("/jpa/users/{username}/todos")
     public List<Todo> getAllTodos(@PathVariable String username){
-        return todoService.findAll();
+        return todoJpaRepository.findByUsername(username);
     }
-    @GetMapping("/users/{username}/todos/{id}")
+    @GetMapping("/jpa/users/{username}/todos/{id}")
     public Todo getTodo(@PathVariable String username, @PathVariable long id){
-        return todoService.findById(id);
+        return todoJpaRepository.findById(id).get();
     }
 
 
 
-    @DeleteMapping("/users/{username}/todos/{id}")
+    @DeleteMapping("/jpa/users/{username}/todos/{id}")
     public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id){
-        Todo todo = todoService.deleteById(id);
-        if(todo != null){
-            return ResponseEntity.noContent().build();
-        }
+        todoJpaRepository.deleteById(id);
 
-        return ResponseEntity.notFound().build();
+        return ResponseEntity.noContent().build();
     }
 
-    @PutMapping("/users/{username}/todos/{id}")
+    @PutMapping("/jpa/users/{username}/todos/{id}")
     public ResponseEntity<Todo> updateTodo(@PathVariable String username,
                                            @PathVariable long id,
                                            @RequestBody Todo todo){
-        todoService.save(todo);
-        return new ResponseEntity<>(todo, HttpStatus.OK);
+        todo.setUsername(username);
+
+        todoJpaRepository.save(todo);
+        return new ResponseEntity<Todo>(todo, HttpStatus.OK);
     }
 
-    @PostMapping("/users/{username}/todos")
+    @PostMapping("/jpa/users/{username}/todos")
     public ResponseEntity<Void> createTodo(@PathVariable String username,
                                            @RequestBody Todo todo){
-        Todo createdTodo = todoService.save(todo);
+        todo.setUsername(username);
+
+        Todo createdTodo = todoJpaRepository.save(todo);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
                                     .path("/{id}").buildAndExpand(createdTodo.getId())
                                     .toUri();
